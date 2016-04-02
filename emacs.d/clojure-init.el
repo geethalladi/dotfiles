4ddeb0cd7ff1ce9f4242fc20585fe968b1bde6e9
zugzwang-dev <zugzwang.dev@gmail.com>
2016-04-02 17:53:42 +0530

    emacs config for ocaml, clojure, lisp

---
+++ emacs.d/clojure-init.el
@@ -0,0 +1,49 @@
+(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+
+(setq nrepl-popup-stacktraces nil)
+
+(add-to-list 'same-window-buffer-names "<em>nrepl</em>")
+
+;; General auto complete
+(require 'auto-complete-config)
+(setq ac-delay 0.0)
+(setq ac-quick-help-delay 0.5)
+(ac-config-default)
+
+;; ac-nrepl
+(require 'ac-nrepl)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+;(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+
+(add-to-list 'ac-modes 'cider-mode)
+;(add-to-list 'ac-modes 'cider-repl-mode)
+
+;; Popping up contextual menu
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
+
+(add-hook 'cider-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+(show-paren-mode 1)
+
+(require 'rainbow-delimiters)
+(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+;(global-rainbow-delimiters-mode)
+
+(setq nrepl-log-messages t)
+(setq nrepl-buffer-name-separator "-")
+
+(require 'icomplete)
+(add-hook 'cider-mode-hook #'company-mode)
+(add-hook 'cider-repl-mode-hook #'company-mode)
+
+(add-hook 'cider-repl-mode-hook '(lambda () (linum-mode 0)))
+
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'subword-mode)
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+
+;; (cider-jack-in)
---
+++ emacs.d/lisp-init.el
@@ -0,0 +1,19 @@
+(require 'slime)
+(slime-setup)
+
+(setq inferior-lisp-program "/usr/local/bin/sbcl --noinform")
+
+(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'lisp-mode-hook 'paredit-mode)
+
+(add-hook 'slime-repl-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'inferior-lisp-mode-hook 'rainbow-delimiters-mode)
+
+;; paredit mode for slime mode
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
