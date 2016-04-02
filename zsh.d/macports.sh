6b19634774d5a3a16a89f5e48577a6c2272fcd62
zugzwang.dev <zugzwang.dev@gmail.com>
2016-04-02 10:08:14 +0530

    MacPorts File

---
+++ zsh.d/macports.sh
@@ -0,0 +1,7 @@
+##
+# Your previous ~/.bash_profile file was backed up as ~/.bash_profile.macports-saved_2015-04-09_at_07:32:07
+##
+
+# MacPorts Installer addition on 2015-04-09_at_07:32:07: adding an appropriate PATH variable for use with MacPorts.
+# export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+# Finished adapting your PATH environment variable for use with MacPorts.
