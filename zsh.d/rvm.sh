2fdd25e6799b601f235188302ee3da44f2ad6fd3
zugzwang.dev <zugzwang.dev@gmail.com>
2016-04-02 10:08:34 +0530

    RVM configuration

---
+++ zsh.d/rvm.sh
@@ -0,0 +1,13 @@
+# RVM Installation
+
+export PATH="${PATH}:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+# USING THE DEFAULT GEMSET WHILE LOGGING IN
+export GEM_HOME=~/.rvm/gems/ruby-2.0.0-p353
+export GEM_PATH=~/.rvm/gems/ruby-2.0.0-p353
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+rvm use ruby-2.0.0-p353; echo # ECHO TO PRINT A NEWLINE AT THE END
+
+[[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
