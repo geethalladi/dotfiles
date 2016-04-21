2020f24d85a666707815e294ce868c2a3c2542a3
zugzwang-dev <zugzwang.dev@gmail.com>
2016-04-21 13:11:05 +0530

    Adding shell_session_update function for rvm

--- rvm.d/rvm.sh
+++ rvm.d/rvm.sh
@@ -1,5 +1,9 @@
 # RVM Installation
 
+# This function definition is required by rvm
+
+shell_session_update() { :; }
+
 # USING THE DEFAULT GEMSET WHILE LOGGING IN
 export GEM_HOME=~/.rvm/gems/ruby-2.0.0-p353
 export GEM_PATH=~/.rvm/gems/ruby-2.0.0-p353
@@ -8,6 +12,7 @@ export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scrip
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
+rvm default
 rvm use ruby-2.0.0-p353@zz-junk; echo # ECHO TO PRINT A NEWLINE AT THE END
 
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
