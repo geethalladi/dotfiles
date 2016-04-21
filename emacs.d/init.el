bb18940b80a924e45b05df616fb861e42d210a84
zugzwang-dev <zugzwang.dev@gmail.com>
2016-04-21 13:12:24 +0530

    Updating custom faces to init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -81,3 +81,9 @@
  '(tool-bar-mode nil))
 
 (setq mac-shift-modifier 'meta)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
