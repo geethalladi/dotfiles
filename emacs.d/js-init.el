80b5ee0049e2b35c07c2991a169a45bf08739dbc
zugzwang-dev <zugzwang.dev@gmail.com>
2016-04-24 09:34:12 +0530

    Adding smartparens mode for js

--- emacs.d/js-init.el
+++ emacs.d/js-init.el
@@ -1,8 +1,7 @@
 ;; ;; JavaScript
 
-(load "~/.emacs.d/lisp-init.el")
+;; (load "~/.emacs.d/lisp-init.el")
 
-(require 'smartparens)
 (require 'js2-mode)
 
 (setq js2-highlight-level 3)
@@ -10,17 +9,19 @@
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-(add-hook 'js2-mode-hook 'ac-js2-mode)
-
-(define-key js-mode-map "{" 'paredit-open-curly)
-(define-key js-mode-map "}" 'paredit-close-curly-and-newline)
-
-(global-set-key [f5] 'slime-js-reload)
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (slime-js-minor-mode 1)))
-(add-hook 'js2-mode-hook
+(require 'smartparens)
+(add-hook 'js-mode-hook 'smartparens-mode)
+;; (add-hook 'js-mode-hook
+;;           (lambda ()
+;;             (slime-js-minor-mode 1)))
+(add-hook 'js-mode-hook
           (lambda ()
             (moz-minor-mode 1)))
+(add-hook 'js-mode-hook 'ac-js2-mode)
 
+;; (define-key js-mode-map "{" 'paredit-open-curly)
+;; (define-key js-mode-map "}" 'paredit-close-curly-and-newline)
+
+;; TODO
+;; (global-set-key [f5] 'slime-js-reload)
 (setq js2-basic-offset 2)
