f723015e6baecd329a4cd20b9dc05f7b9d8ffcb8
zugzwang-dev <zugzwang.dev@gmail.com>
2016-05-19 17:56:48 +0530

    Switching to ruby 2.2.3

--- rvm.d/rvm.sh
+++ rvm.d/rvm.sh
@@ -5,14 +5,14 @@
 shell_session_update() { :; }
 
 # USING THE DEFAULT GEMSET WHILE LOGGING IN
-export GEM_HOME=~/.rvm/gems/ruby-2.0.0-p353
-export GEM_PATH=~/.rvm/gems/ruby-2.0.0-p353
+export GEM_HOME=~/.rvm/gems/ruby-2.2.3
+export GEM_PATH=~/.rvm/gems/ruby-2.2.3
 
 export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scripting
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 rvm default
-rvm use ruby-2.0.0-p353@zz-junk; echo # ECHO TO PRINT A NEWLINE AT THE END
+rvm use default@zz-junk; echo # ECHO TO PRINT A NEWLINE AT THE END
 
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
