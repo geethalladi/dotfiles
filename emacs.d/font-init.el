0c63cf23a4d00b942159c2aa3eed2fc5738ebda8
zugzwang-dev <zugzwang.dev@gmail.com>
2016-05-19 17:58:47 +0530

    Changing the font face AGAIN!!

--- emacs.d/font-init.el
+++ emacs.d/font-init.el
@@ -1,7 +1,7 @@
 ;; FONT INIT
 ;; (set-face-attribute 'default nil :font "Monaco-15")
 
-;; (set-face-attribute 'default nil :font "Monaco-14:spacing=110")
+;; (set-face-attribute 'default nil :font "Monaco-13:spacing=110")
 
 ;; (set-face-attribute 'default nil :font "Inconsolata-13")
 (set-face-attribute 'default nil :font "Inconsolata-dz for Powerline:pixelsize=13:spacing=110")
