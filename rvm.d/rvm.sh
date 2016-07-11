97be96f3551b6b5deaff35107db1f06339319cc4
zugzwang-dev <zugzwang.dev@gmail.com>
2016-07-11 09:03:47 +0530

    Commenting out GEM_HOME and GEM_PATH

--- rvm.d/rvm.sh
+++ rvm.d/rvm.sh
@@ -5,14 +5,16 @@
 shell_session_update() { :; }
 
 # USING THE DEFAULT GEMSET WHILE LOGGING IN
-export GEM_HOME=~/.rvm/gems/ruby-2.2.3
-export GEM_PATH=~/.rvm/gems/ruby-2.2.3
+# export GEM_HOME=~/.rvm/gems/ruby-2.2.3
+# export GEM_PATH=~/.rvm/gems/ruby-2.2.3
 
 export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scripting
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 rvm default
-rvm use default@zz-junk; echo # ECHO TO PRINT A NEWLINE AT THE END
+
+# Not required if a gemset is marked as --default
+rvm use default@zz-junk
 
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
