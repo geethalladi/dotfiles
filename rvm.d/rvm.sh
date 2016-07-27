17172df1b589e1efc4b5c13c186b3baf3872db38
zugzwang-dev <zugzwang.dev@gmail.com>
2016-07-27 17:27:02 +0530

    RVM functions are required
    - Need to source in these to get the rvm functions
    - No getting away from them

--- rvm.d/rvm.sh
+++ rvm.d/rvm.sh
@@ -4,16 +4,15 @@
 
 shell_session_update() { :; }
 
-# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-[[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
-
-# rvm default
-
-# # Not required if a gemset is marked as --default
-# rvm use default@zz-junk
-
-
 # USING THE DEFAULT GEMSET WHILE LOGGING IN
 export GEM_HOME=~/.rvm/gems/ruby-2.3.0
 export GEM_PATH=~/.rvm/gems/ruby-2.3.0
 export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scripting
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+[[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
+
+rvm default
+
+# # Not required if a gemset is marked as --default
+rvm use default@zz-junk --default
