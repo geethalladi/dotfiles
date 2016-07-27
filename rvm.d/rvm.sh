6ff21ebadac87ec1e7804a849507a0c92f1f7243
zugzwang-dev <zugzwang.dev@gmail.com>
2016-07-27 16:40:04 +0530

    Commenting out unused lines in rvm
    - Default settings work well, when the default gemset is set properly

--- rvm.d/rvm.sh
+++ rvm.d/rvm.sh
@@ -4,17 +4,16 @@
 
 shell_session_update() { :; }
 
-# USING THE DEFAULT GEMSET WHILE LOGGING IN
-# export GEM_HOME=~/.rvm/gems/ruby-2.3.0
-# export GEM_PATH=~/.rvm/gems/ruby-2.3.0
-
-export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scripting
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
-rvm default
-
-# Not required if a gemset is marked as --default
-rvm use default@zz-junk
-
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
+
+# rvm default
+
+# # Not required if a gemset is marked as --default
+# rvm use default@zz-junk
+
+
+# USING THE DEFAULT GEMSET WHILE LOGGING IN
+export GEM_HOME=~/.rvm/gems/ruby-2.3.0
+export GEM_PATH=~/.rvm/gems/ruby-2.3.0
+export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scripting
