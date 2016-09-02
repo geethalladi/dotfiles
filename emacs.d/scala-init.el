16fad14707c01d1fa16f70fa8ad46d5964d8d71b
zugzwang-dev <zugzwang.dev@gmail.com>
2016-09-02 12:27:32 +0530

    Adding smartparens for scala-mode

--- emacs.d/scala-init.el
+++ emacs.d/scala-init.el
@@ -1,7 +1,13 @@
 ;; (use-package ensime :pin melpa-stable)
 ;; (add-to-list exec-path "/usr/local/bin")
 
+(add-to-list 'auto-mode-alist '("\\.sc" . scala-mode))
 (add-to-list 'auto-mode-alist '("\\.scala" . scala-mode))
 
 (require 'ensime)
+(require 'smartparens)
+(require 'yafolding)
+
+(add-hook 'scala-mode-hook 'smartparens-mode)
+(add-hook 'scala-mode-hook 'yafolding-mode)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
