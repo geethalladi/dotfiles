304bdaab1fe81b9306cb920ee9c64c708f1ecfa8
zugzwang-dev <zugzwang.dev@gmail.com>
2016-09-16 13:05:13 +0530

    Updating scala mode

--- emacs.d/scala-init.el
+++ emacs.d/scala-init.el
@@ -4,10 +4,12 @@
 (add-to-list 'auto-mode-alist '("\\.sc" . scala-mode))
 (add-to-list 'auto-mode-alist '("\\.scala" . scala-mode))
 
+(setq scala-indent:align-parameters t)
+
 (require 'ensime)
 (require 'smartparens)
 (require 'yafolding)
 
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 (add-hook 'scala-mode-hook 'smartparens-mode)
 (add-hook 'scala-mode-hook 'yafolding-mode)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
