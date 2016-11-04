49081668496e663495b010b034b9d678813c667e
zugzwang-dev <zugzwang.dev@gmail.com>
2016-11-04 10:08:15 +0530

    Updating scala init file

--- emacs.d/scala-init.el
+++ emacs.d/scala-init.el
@@ -1,7 +1,7 @@
 ;; (use-package ensime :pin melpa-stable)
 ;; (add-to-list exec-path "/usr/local/bin")
 
-(add-to-list 'auto-mode-alist '("\\.sc" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.sc$" . scala-mode))
 (add-to-list 'auto-mode-alist '("\\.scala" . scala-mode))
 
 (setq scala-indent:align-parameters t)
