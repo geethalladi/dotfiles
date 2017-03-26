b916cd598a802054d71a0694d087a4afc6fb03c6
zugzwang-dev <zugzwang.dev@gmail.com>
2017-03-26 09:19:22 +0530

    Updating python mode hook

--- emacs.d/py-init.el
+++ emacs.d/py-init.el
@@ -3,7 +3,21 @@
 (add-hook 'python-mode-hook
  (function (lambda ()
              (setq indent-tabs-mode nil
-                   python-indent 2
-                   tab-width 2))))
+                   python-indent 4
+                   tab-width 4))))
+(setq python-indent 4)
+
+(elpy-enable)
+(add-hook 'python-mode-hook 'eldoc-mode)
 (add-hook 'python-mode-hook 'smartparens-mode)
 (add-hook 'python-mode-hook 'yafolding-mode)
+(add-hook 'python-mode-hook 'flycheck-mode)
+(add-hook 'python-mode-hook 'yas-minor-mode)
+
+;; (add-hook 'python-mode-hook 'anaconda-mode)
+;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+
+(defun projectile-pyenv-mode-set ()
+  "Set pyenv version matching project name.
+  Version must be already installed."
+  (pyenv-mode-set (projectile-project-name)))
