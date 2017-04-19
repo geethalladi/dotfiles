62e4a27121423fa26948a90fd198a5f60d9e50a1
zugzwang-dev <zugzwang.dev@gmail.com>
2017-04-19 11:46:55 +0530

    Ipython integration with emacs

--- emacs.d/py-init.el
+++ emacs.d/py-init.el
@@ -7,6 +7,33 @@
                    tab-width 4))))
 (setq python-indent 4)
 
+(defun projectile-pyenv-mode-set ()
+  "Set pyenv version matching project name.
+  Version must be already installed."
+  (pyenv-mode-set (projectile-project-name)))
+
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--simple-prompt -i")
+
+;; Remove autocomplete mode for python
+(setq ac-modes (delq 'python-mode ac-modes))
+
 (elpy-enable)
 (add-hook 'python-mode-hook 'eldoc-mode)
 (add-hook 'python-mode-hook 'smartparens-mode)
@@ -16,8 +43,3 @@
 
 ;; (add-hook 'python-mode-hook 'anaconda-mode)
 ;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-
-(defun projectile-pyenv-mode-set ()
-  "Set pyenv version matching project name.
-  Version must be already installed."
-  (pyenv-mode-set (projectile-project-name)))
