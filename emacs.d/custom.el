07a3aa42e080eef59dde6f9cce3eb42c3a6abb95
zugzwang-dev <zugzwang.dev@gmail.com>
2017-10-26 17:46:41 +0530

    Updating custom variables in init.el

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -49,3 +49,19 @@
 
 ;; Setting key for global multiple cursor mode
 (global-set-key (kbd "C-c m c") 'mc/edit-lines)
+
+;; '(custom-safe-themes
+;;   (quote
+;;    ("e14f8791b180b69f95550ae9fb6b3fa2321115197908f0a8a2cd924bd5acc86f"
+;;     "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0"
+;;     "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
+;;     "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d"
+;;     default)))
+;; '(ecb-layout-window-sizes
+;;   (quote
+;;    (("left8"
+;;      (ecb-directories-buffer-name 0.27722772277227725 . 0.2962962962962963)
+;;      (ecb-sources-buffer-name 0.27722772277227725 . 0.24074074074074073)
+;;      (ecb-methods-buffer-name 0.27722772277227725 . 0.25925925925925924)
+;;      (ecb-history-buffer-name 0.27722772277227725 . 0.18518518518518517)))))
+;; '(ecb-options-version "2.40")
