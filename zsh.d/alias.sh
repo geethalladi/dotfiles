67e9efedca5afbca1f9d51d75ef288234dce770c
zugzwang-dev <zugzwang.dev@gmail.com>
2017-12-12 11:25:48 +0530

    Moving alias into separate config

---
+++ zsh.d/alias.sh
@@ -0,0 +1,33 @@
+########################################################################################
+# Alias definitions
+
+alias c='clear'
+alias l='ls -al'
+alias ls='ls -GFh'
+
+# Emacs to use cocoa emacs app
+# alias emacs=/Applications/Emacs.app/Contents/MacOS/Emacs
+
+alias e='ec'
+
+# alias emacsclient to 'ecl'
+alias ecl='emacsclient -nw'
+
+alias m='mvn -Dmaven.javadoc.skip=true'
+alias g='git'
+alias t='tree'
+alias pass='openssl rand -base64 16'
+alias rg='rvm gemset use'
+
+
+alias scheme="rlwrap -r -c -f ~/extn.d/mit_scheme_bindings.txt scheme"
+alias ghci="ghci -XNPlusKPatterns"
+alias hivec="hive -hiveconf hive.root.logger=DEBUG,console"
+alias jnote="jupyter notebook --notebook-dir=~/repo/notebooks"
+
+function show() {
+    echo "rvm: "`rvm-prompt`
+    echo "git branch: "`git_current_branch`
+    git-config.sh
+}
+alias shw='show'
