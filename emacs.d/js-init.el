e759ade4702a45dd70f4ea59db5e6d0f20179761
zugzwang-dev <zugzwang.dev@gmail.com>
2018-01-23 11:16:35 +0530

    Keybinding for jsdoc comments

--- emacs.d/js-init.el
+++ emacs.d/js-init.el
@@ -89,3 +89,8 @@
     (turn-on-purescript-indentation)))
 
 (setq psc-ide-use-npm-bin t)
+
+ (add-hook 'js2-mode-hook
+           #'(lambda ()
+               (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
+               (define-key js2-mode-map "@" 'js-doc-insert-tag)))
