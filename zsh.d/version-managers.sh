c501b14320a7463a6f6cd46c8d56b09b6644e0cc
zugzwang-dev <zugzwang.dev@gmail.com>
2018-01-23 13:41:03 +0530

    Conditionally sourcing version managers

--- zsh.d/version-managers.sh
+++ zsh.d/version-managers.sh
@@ -1,10 +1,20 @@
-echo "Sourcing nvm/rvm/gvm scripts..."
+# echo "Sourcing nvm/rvm/gvm scripts..."
 # Updating the path here after zshrc
-source ~/.gvm/scripts/gvm
+
+if [[ ! -v GVM_VERSION ]]; then
+    echo "Sourcing gvm scripts..."
+    source ~/.gvm/scripts/gvm
+fi
 
 # NVM PATH
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+if [[ ! -v NVM_DIR ]]; then
+    echo "Sourcing nvm scripts..."
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+fi
 
 # RVM should be the final one
-source ${DOTFILE_DIR_RVM}/rvm.sh
+if [[ ! -v rvm_version ]]; then
+    echo "Sourcing rvm scripts..."
+    source ${DOTFILE_DIR_RVM}/rvm.sh
+fi
