1ad6bd51b6de4e7b6099e779b72389fe29de3117
zugzwang-dev <zugzwang.dev@gmail.com>
2018-02-09 15:16:04 +0530

    Using state to source in xvm

--- zsh.d/xvm.sh
+++ zsh.d/xvm.sh
@@ -1,29 +1,18 @@
 # echo "Sourcing nvm/rvm/gvm scripts..."
 # Updating the path here after zshrc
 
-echo "Sourcing version managers"
+if [ "${XVM_INIT_COMPLETE}" != 1 ]; then
+    echo "Sourcing version managers"
 
-source ~/.gvm/scripts/gvm
+    # Go Version manager
+    source ~/.gvm/scripts/gvm
 
-# This loads nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    # nvm
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-source ${DOTFILE_DIR_RVM}/rvm.sh
+    # rvm
+    source ${DOTFILE_DIR_RVM}/rvm.sh
 
-# GVM_PATH=$(which gvm)
-# if [[ ${GVM_PATH} == "gvm not found" ]]; then
-#     source ~/.gvm/scripts/gvm
-# fi
-
-# NVM_PATH=$(which nvm)
-# if [[ ${NVM_PATH} == "nvm not found" ]]; then
-#     export NVM_DIR="$HOME/.nvm"
-#     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# fi
-
-# # RVM should be the final one
-# RVM_PATH=$(which rvm)
-# if [[ ${RVM_PATH} == "rvm not found" ]]; then
-#     source ${DOTFILE_DIR_RVM}/rvm.sh
-# fi
+    export XVM_INIT_COMPLETE=1
+fi
