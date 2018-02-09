b9a9f0128332bd3b7bf5e9c44b15e96ca167691b
zugzwang-dev <zugzwang.dev@gmail.com>
2018-02-09 15:07:57 +0530

    Customization in init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,6 +15,11 @@
 (add-hook 'after-init-hook (lambda () (load "~/.emacs.d/after-init.el")))
 
 (add-hook 'before-make-frame-hook (lambda () (load "~/.emacs.d/after-init.el")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Automatic Customizations go here ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -23,7 +28,11 @@
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "82b67c7e21c3b12be7b569af7c84ec0fb2d62105629a173e2479e1053cff94bd" "cd560f7570de0dcdcf06953b3f1a25145492a54f100f9c8da3b4091b469f7f02" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
+     "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147"
+     "82b67c7e21c3b12be7b569af7c84ec0fb2d62105629a173e2479e1053cff94bd"
+     "cd560f7570de0dcdcf06953b3f1a25145492a54f100f9c8da3b4091b469f7f02"
+     "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(org-agenda-files nil)
  '(package-selected-packages (quote (package-build shut-up epl git commander f dash s)))
@@ -36,4 +45,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(js2-function-param ((t (:foreground "Brown"))))
+ '(org-done ((t (:foreground "ForestGreen" :weight normal :strike-through t))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "LightSalmon" :strike-through t)))))
