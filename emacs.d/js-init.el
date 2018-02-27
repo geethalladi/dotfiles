6d75ad40451580510d6a78d4167483cd001ee846
geethalladi <geeth@freshdesk.com>
2018-02-27 18:46:31 +0530

    Making xref work

--- emacs.d/js-init.el
+++ emacs.d/js-init.el
@@ -3,17 +3,20 @@
 ;; (load "~/.emacs.d/lisp-init.el")
 
 (require 'js2-mode)
-(require 'js-doc)
 (require 'smartparens)
+
 (require 'js2-refactor)
 (require 'discover-js2-refactor)
+
+(require 'js-doc)
 (require 'mocha)
+(require 'nvm)
+
 (require 'tern)
 (require 'nodejs-repl)
-(require 'nvm)
 (require 'xref-js2)
 
-;; (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (setq js2-highlight-level 3)
@@ -47,7 +50,9 @@
 
 (add-hook 'js2-mode-hook 'tern-mode)
 (add-hook 'js-mode-hook 'smartparens-mode)
-(add-hook 'js-mode-hook 'ac-js2-mode)
+
+;; (add-hook 'js-mode-hook 'ac-js2-mode)
+
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
 (add-hook 'js-mode-hook 'js-mode-configuration-hook)
 (add-hook 'json-mode 'flymake-json-load)
@@ -62,16 +67,17 @@
               (define-key js2-mode-map "\C-ct" 'mocha-test-at-point)))
 
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
+
 ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
 ;; unbind it.
-(define-key js-mode-map (kbd "M-.") nil)
-
-(add-hook 'js2-mode-hook (lambda ()
-  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
-
 (js2r-add-keybindings-with-prefix "C-c C-r")
 
 (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+(define-key js2-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
 ;; (add-hook 'js-mode-hook
 ;;           (lambda ()
