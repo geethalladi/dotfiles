c80d82a6076d8fc12548afe419102a09b41031bb
geethalladi <geeth@freshdesk.com>
2018-03-14 16:12:16 +0530

    Again updating font. Making it smaller this time around

--- emacs.d/font-init.el
+++ emacs.d/font-init.el
@@ -15,7 +15,9 @@
 (load "~/.emacs.d/font-ligatures.el")
 
 ;; (set-face-attribute 'default nil :font "Hasklig:pixelsize=13:spacing=110:weight=semi-bold")
-(set-face-attribute 'default nil :font "Hasklig:pixelsize=16:spacing=110:weight=normal")
+;; (set-face-attribute 'default nil :font "Hasklig:pixelsize=16:spacing=110:weight=normal")
+(set-face-attribute 'default nil :font "Hasklig:pixelsize=15:spacing=110:weight=medium")
+
 
 ;; (require 'color-identifiers-mode)
 ;; (let ((faces '(font-lock-comment-face
