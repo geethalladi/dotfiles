b1de44847bcf5a00f70c1d32d3663b9a41b47523
geethalladi <geeth@freshdesk.com>
2018-04-28 07:59:30 +0530

    Some more font changes

--- emacs.d/font-init.el
+++ emacs.d/font-init.el
@@ -16,8 +16,9 @@
 
 ;; (set-face-attribute 'default nil :font "Hasklig:pixelsize=13:spacing=110:weight=semi-bold")
 ;; (set-face-attribute 'default nil :font "Hasklig:pixelsize=16:spacing=110:weight=normal")
-(set-face-attribute 'default nil :font "Hasklig:pixelsize=15:spacing=110:weight=medium")
-
+(if (window-system)
+    (set-face-attribute 'default nil :font "Consolas:pixelsize=16:spacing=110:weight=normal")
+  (set-face-attribute 'default nil :font "Hasklig:pixelsize=13:spacing=110:weight=medium"))
 
 ;; (require 'color-identifiers-mode)
 ;; (let ((faces '(font-lock-comment-face
