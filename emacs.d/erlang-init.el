8e17d4b70f1023f4d43549b113589d69ce2802e6
geethalladi <geeth@freshdesk.com>
2018-04-30 11:24:31 +0530

    Adding erlang major mode

---
+++ emacs.d/erlang-init.el
@@ -0,0 +1,6 @@
+(require 'erlang)
+(require 'erlang-start)
+
+;; hard-coding the erlang root dir for now
+;; Should be obtaining this from $ERLANG_HOME
+(setq erlang-root-dir "/usr/local/Cellar/erlang/20.3.4")
