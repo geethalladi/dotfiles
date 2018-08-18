bac891bf57b41cb4eba2325bb1d94bc902aeda3f
geethalladi <geeth@freshdesk.com>
2018-08-18 16:11:59 +0530

    Adding typescript init file

---
+++ emacs.d/ts-init.el
@@ -0,0 +1,16 @@
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
