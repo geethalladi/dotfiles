ad2a81e7631dfd6cdbeb8cab52e39837d5ac2e1e
geethalladi <geeth@freshdesk.com>
2018-10-27 13:49:40 +0530

    Refactoring clojure-init

--- emacs.d/clojure-init.el
+++ emacs.d/clojure-init.el
@@ -1,33 +1,25 @@
-(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
-
-(setq nrepl-popup-stacktraces nil)
-
-(add-to-list 'same-window-buffer-names "<em>nrepl</em>")
-
-;; Popping up contextual menu
-(eval-after-load "cider"
-  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
-
-(add-hook 'cider-mode-hook 'paredit-mode)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(require 'rainbow-delimiters)
+(require 'icomplete)
 
 (show-paren-mode 1)
-
-(require 'rainbow-delimiters)
-(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+(add-to-list 'same-window-buffer-names "<em>nrepl</em>")
 
 (setq nrepl-log-messages t)
 (setq nrepl-buffer-name-separator "-")
+(setq nrepl-popup-stacktraces nil)
 
-(require 'icomplete)
+(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-mode-hook 'paredit-mode)
 (add-hook 'cider-mode-hook #'company-mode)
-(add-hook 'cider-repl-mode-hook #'company-mode)
 
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-repl-mode-hook '(lambda () (linum-mode 0)))
 
 (add-hook 'clojure-mode-hook #'paredit-mode)
 (add-hook 'clojure-mode-hook #'subword-mode)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook #'turn-on-eldoc-mode)
 
 ;; (cider-jack-in)
