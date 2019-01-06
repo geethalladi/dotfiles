c04c325c2dd8042d5348e1be4bc69d7956d62397
geethalladi <geeth@freshdesk.com>
2019-01-06 07:42:08 +0530

    Dark Mode creates a lot of reflection. Still not good yet !!!

--- emacs.d/font-theme-init.el
+++ emacs.d/font-theme-init.el
@@ -17,12 +17,9 @@
 ;; (when (not window-system)
 ;;  (load-theme 'tango-dark t))
 
-;; Switching to dark theme again
-(load-theme 'leuven t)
-
-;; (if (window-system)
-;;     (load-theme 'leuven t)
-;;   (load-theme 'tango-dark t))
+(if (window-system)
+    (load-theme 'leuven t)
+  (load-theme 'tango-dark t))
 
 ;; FONT INIT
 
