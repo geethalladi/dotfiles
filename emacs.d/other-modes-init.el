03e87d3668bf3a3561677ccd44f30d06aedfeefc
geethalladi <geeth@freshdesk.com>
2019-01-07 06:25:51 +0530

    Moving smaller-modes to other-modes-init

---
+++ emacs.d/other-modes-init.el
@@ -0,0 +1,22 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; YAML mode configurations ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'yaml-mode)
+(add-hook 'yaml-mode-hook 'highlight-indentation-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; REST Client mode configurations ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'restclient)
+
+(add-to-list 'auto-mode-alist '("\\.\\(rest\\)$" . restclient-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(http\\)$" . restclient-mode))
+
+(add-hook 'restclient-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq tab-width 2)))
+(add-hook 'restclient-mode-hook 'smartparens-mode)
+
+;; (add-to-list 'company-backends 'company-restclient)
+;; (setq tab-always-indent nil)
