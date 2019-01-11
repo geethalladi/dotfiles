ccc84a09dd32cef1182374c1594eac13db99ef1a
geethalladi <geeth@freshdesk.com>
2019-01-11 19:46:58 +0530

    Adding company mode

---
+++ emacs.d/company-init.el
@@ -0,0 +1,38 @@
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 3)
+  (global-company-mode t))
+
+;; (use-package company-irony
+;;   :ensure t
+;;   :config
+;;   (add-to-list 'company-backends 'company-irony))
+
+;; (use-package irony
+;;   :ensure t
+;;   :config
+;;   (add-hook 'c++-mode-hook 'irony-mode)
+;;   (add-hook 'c-mode-hook 'irony-mode)
+;;   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+;; (use-package irony-eldoc
+;;   :ensure t
+;;   :config
+;;   (add-hook 'irony-mode-hook #'irony-eldoc))
+
+;; (use-package company-jedi
+;;   :ensure t
+;;   :config
+;;   (add-hook 'python-mode-hook 'jedi:setup))
+
+;; (defun my/python-mode-hook ()
+;;   (add-to-list 'company-backends 'company-jedi))
+
+;; (add-hook 'python-mode-hook 'my/python-mode-hook)
+
+;; company box mode
+;; (use-package company-box
+;;   :ensure t
+;;   :hook (company-mode . company-box-mode))
