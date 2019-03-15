eae7a79dc6dbbf0da65069f77616a24da88761fe
geethalladi <geeth@freshdesk.com>
2019-03-15 06:38:06 +0530

    Custom configurations for TRAMP

---
+++ emacs.d/tramp-init.el
@@ -0,0 +1,8 @@
+;; custom configurations for tramp
+(setq remote-file-name-inhibit-cache nil)
+(setq vc-ignore-dir-regexp
+      (format "%s\\|%s"
+                    vc-ignore-dir-regexp
+                    tramp-file-name-regexp))
+(setq tramp-verbose 1)
+(setq tramp-default-method "ssh")
