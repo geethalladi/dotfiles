0df1cfed8582658ae35afb8fc20c195830c81705
geethalladi <geeth.alladi@freshworks.com>
2019-07-28 21:28:11 +0530

    Creating an abstraction for adding browser-capture-templates

--- emacs.d/org-capture-init.el
+++ emacs.d/org-capture-init.el
@@ -9,19 +9,29 @@
 
 (setq self/notes-directory "~/Desktop/notes/")
 
-(setq
- org-capture-templates
- (let ((notes-file (concat self/notes-directory "browser.org"))) `(
-  ("p" "Protocol" entry (file+headline ,notes-file "Notes")
-   "* %^{Title}\n\
+(defun self--add-capture-templates ()
+  "Add the templates needed for org-capture from firefox browser"
+
+  (let* ((notes-file (concat self/notes-directory "browser.org"))
+         (browser-capture-templates
+          `(("p" "Protocol" entry (file+headline notes-file "Notes")
+             "* %^{Title}\n\
 Source: [[%:link][%:description]], %(progn (setq self/delete-frame-after-capture 2) \"\")\n\
 Captured On: %u\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n"
-   :empty-lines 1)
-
-  ("L" "Protocol Link" entry (file+headline ,notes-file "Notes")
-   "* %?[[%:link][%:description]] %(progn (setq self/delete-frame-after-capture 2) \"\")\n\
+             :empty-lines 1)
+            ("L" "Protocol Link" entry (file+headline ,notes-file "Notes")
+             "* %?[[%:link][%:description]] %(progn (setq self/delete-frame-after-capture 2) \"\")\n\
 Captured On: %u"
-   :empty-lines 1))))
+             :empty-lines 1))))
+
+    (if (boundp 'org-capture-templates)
+        ;; append to the list
+        (setq org-capture-templates
+              (append org-capture-templates browser-capture-templates))
+      ;; create the variable
+      (setq org-capture-templates browser-capture-templates))))
+
+(self--add-capture-templates)
 
 ;; (defun self/transform-square-brackets-to-round-ones(string-to-transform)
 ;;   "Transforms [ into ( and ] into ), other chars left unchanged."
