02c768c7c9e19e0e256830caa84a52f90f7524b2
geethalladi <geeth.alladi@freshworks.com>
2019-07-29 10:10:33 +0530

    Updating minor modes for emacs-lisp and lisp-interaction mode

--- emacs.d/emacs-lisp-init.el
+++ emacs.d/emacs-lisp-init.el
@@ -1,4 +1,15 @@
 ;; Customizations for emacs lisp mode
 
-(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+(defun self/emacs-lisp-mode ()
+  "Customisation of emacs lisp mode"
+  (smartparens-mode)
+  (rainbow-delimiters-mode))
+
+(add-hook 'emacs-lisp-mode-hook 'self/emacs-lisp-mode)
+
+(defun self/lisp-interaction-mode ()
+  "Customizations of Lisp Interaction mode"
+  (smartparens-mode)
+  (rainbow-delimiters-mode))
+
+(add-hook 'lisp-interaction-mode-hook 'self/lisp-interaction-mode)
