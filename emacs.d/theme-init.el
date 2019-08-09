a26f42c0daf3ac902fdc5064b19eccf7ff8c26de
geethalladi <geeth.alladi@freshworks.com>
2019-08-09 13:10:18 +0530

    Moving to font size 16

--- emacs.d/theme-init.el
+++ emacs.d/theme-init.el
@@ -117,7 +117,7 @@
 ;; (set-face-attribute 'default nil :font "Fira Code:pixelsize=15:spacing=110:weight=normal")
 ;; (load "~/.emacs.d/vendor/fira-code.el")
 
-(set-face-attribute 'default nil :font "Consolas:pixelsize=14:spacing=110:weight=normal")
+(set-face-attribute 'default nil :font "Consolas:pixelsize=16:spacing=110:weight=normal")
 
 ;; Load theme after fonts, to override conflicting customisations in favour of theme
 ;; (if (window-system)
