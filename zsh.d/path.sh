e0a6f15b34a44ec0c8517b1e8404281ef3223835
geethalladi <geeth.alladi@freshworks.com>
2020-01-08 19:21:12 +0530

    Cleaning up PATH variables

--- zsh.d/path.sh
+++ zsh.d/path.sh
@@ -2,15 +2,15 @@
 # PATH Configurations
 
 if [ "$PATH_STATE_UPDATED" -ne 1 ]; then
-    export PATH=${CASK_PATH}:${COREUTIL_PATH}/bin:/usr/local/sbin:${MACPORT_HOME}/bin:${CONSCRIPT_HOME}/bin:${CABAL_HOME}/bin:${GOROOT}/bin:${PATH}:${CONDA_HOME}/bin:${RUBY_SCRIPTS_HOME}:${ANT_HOME}/bin:${GRADLE_HOME}/bin:${HASKELL_HOME}/bin:${ES_HOME}/bin:${EXTN_SCRIPTS_DIR}:${MAVEN_HOME}/bin:
+    export PATH=${CASK_PATH}:${COREUTIL_PATH}/bin:${CONSCRIPT_HOME}/bin:${CABAL_HOME}/bin:${PATH}:${CONDA_HOME}/bin:${RUBY_SCRIPTS_HOME}:${ANT_HOME}/bin:${GRADLE_HOME}/bin:${HASKELL_HOME}/bin:${ES_HOME}/bin:${EXTN_SCRIPTS_DIR}:${MAVEN_HOME}/bin:${GOROOT}/bin
 
     # export PATH=${PATH}:${HADOOP_HOME}/bin:${HIVE_HOME}/bin:${SPARK_HOME}/bin
 
     # Using custom installed java/scala
     export PATH=${JAVA_HOME}/bin:${SCALA_HOME}/bin:${PATH}
-    export PATH=${PATH}:${LOCAL_BIN_DIR}:/usr/local/opt/mysql@5.6/bin/
-    export PATH="${PATH}:${HOME}/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
-    export PATH="/usr/local/bin:${PATH}"
+    export PATH=${PATH}:${LOCAL_BIN_DIR}:/usr/local/opt/mysql@5.6/bin/:/usr/local/sbin
+    export PATH=${PATH}:${HOME}/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin
+    # export PATH="/usr/local/bin:${PATH}"
 
     # for tmuxinator
     # source ${DOTFILE_DIR_TMUX}/tmuxinator.zsh
@@ -20,15 +20,15 @@ if [ "$PATH_STATE_UPDATED" -ne 1 ]; then
 
     # Dont update once everything is in place
     export PATH_STATE_UPDATED=1
+
+    if [[ ${TERM_PROGRAM} != "iTerm.app" && ${TERM} != "eterm-color" && ${TERM} != "dumb" ]]; then
+        source ${DOTFILE_DIR_ZSH}/xvm.sh
+    fi
+
+    # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+    export PATH="$PATH:$HOME/.rvm/bin"
+
+    # Rehash to take care of the PATH variable update
+    # https://stackoverflow.com/questions/36543707/why-does-zsh-ignore-the-path-entry-order
+    rehash
 fi
-
-if [[ ${TERM_PROGRAM} != "iTerm.app" && ${TERM} != "eterm-color" && ${TERM} != "dumb" ]]; then
-    source ${DOTFILE_DIR_ZSH}/xvm.sh
-fi
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-# Rehash to take care of the PATH variable update
-# https://stackoverflow.com/questions/36543707/why-does-zsh-ignore-the-path-entry-order
-rehash
