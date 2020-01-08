150ddbd5a6cee2c42717a0b1f302dd49d8772d95
geethalladi <geeth.alladi@freshworks.com>
2020-01-08 18:06:57 +0530

    No longer manually setting GEM_HOME and GEM_PATH

--- rvm.d/rvm.sh
+++ rvm.d/rvm.sh
@@ -5,9 +5,9 @@
 shell_session_update() { :; }
 
 # USING THE DEFAULT GEMSET WHILE LOGGING IN
-export GEM_HOME=~/.rvm/gems/ruby-2.4.0
-export GEM_PATH=~/.rvm/gems/ruby-2.4.0
-export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scripting
+# export GEM_HOME=~/.rvm/gems/ruby-2.4.0
+# export GEM_PATH=~/.rvm/gems/ruby-2.4.0
+# export PATH="${GEM_HOME}/bin:$HOME/.rvm/bin:${PATH}" # Add RVM to PATH for scripting
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
