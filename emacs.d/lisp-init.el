76455cea4f9d2843b9ebe66cf6f5d6c43bdbfe17
geethalladi <geeth.alladi@freshworks.com>
2020-01-21 18:14:09 +0530

    Adding lisp-minor-mode and racket-mode

--- emacs.d/lisp-init.el
+++ emacs.d/lisp-init.el
@@ -1,7 +1,12 @@
-(defun self/scheme-mode ()
-  "Customizations of scheme mode"
+(defun self/lisp-customizations ()
+  "Customizations of lisp mode"
   (smartparens-mode)
   (rainbow-delimiters-mode))
+
+(defun self/racket-mode ()
+  "Customizations of racket mode"
+  (self/lisp-customizations)
+  (geiser-mode))
 
 (load (expand-file-name "~/installed.d/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
@@ -34,9 +39,14 @@
 
 (setq scheme-program-name "petite")
 
-(setq geiser-active-implementations '(chez))
+;; (setq geiser-active-implementations '(chez))
 
-(setq geiser-default-implementation 'chez)
+;; (setq geiser-default-implementation 'chez)
+
+(setq geiser-implementations-alist
+      '(((regexp "\\.scm$") chez)
+        ((regexp "\\.ss$") racket)
+        ((regexp "\\.rkt$") racket)))
 
 ;; Disable version check
 (setq geiser-repl-skip-version-check-p 't)
@@ -46,8 +56,11 @@
 (require 'geiser)
 (require 'quack)
 
-(add-hook 'scheme-mode-hook 'self/scheme-mode)
+(add-hook 'scheme-mode-hook 'self/lisp-customizations)
+(add-hook 'racket-mode-hook 'self/racket-mode)
 
 (add-to-list 'auto-mode-alist '("\\.scm$" . scheme-mode))
 
 (add-to-list 'auto-mode-alist '("\\.ss$" . scheme-mode))
+
+(add-to-list 'auto-mode-alist '("\\.rkt$" . racket-mode))
