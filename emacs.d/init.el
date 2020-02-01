083cacbdb44229e9f9bb0dc1d90c5d83115c3955
geethalladi <geeth.alladi@freshworks.com>
2020-02-01 11:11:22 +0530

    Cleaning up init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,4 @@
 ;; POST INIT HOOK
-(add-hook 'after-init-hook (lambda () (load "~/.emacs.d/after-init.el")))
 
-;; (add-hook 'before-make-frame-hook (lambda () (load "~/.emacs.d/after-init.el")))
+(add-hook 'after-init-hook
+          (lambda () (load "~/.emacs.d/after-init.el")))
