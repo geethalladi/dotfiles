6405ecf826598a3fdd616470147302fcb74ee05b
geethalladi <geeth.alladi@freshworks.com>
2020-04-05 18:02:16 +0530

    Extracting adding cider to a separate function

--- emacs.d/clojure-init.el
+++ emacs.d/clojure-init.el
@@ -2,30 +2,36 @@
 (require 'icomplete)
 
 (show-paren-mode 1)
-(add-to-list 'same-window-buffer-names "<em>nrepl</em>")
 
-(setq nrepl-log-messages t)
-(setq nrepl-buffer-name-separator "-")
-(setq nrepl-popup-stacktraces nil)
-
-;; no longer prompt while asking for doc-at-point
-(setq cider-prompt-for-symbol nil)
-
-(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'cider-mode-hook 'smartparens-mode)
-(add-hook 'cider-mode-hook #'company-mode)
-
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'cider-repl-mode-hook 'smartparens-mode)
-(add-hook 'cider-repl-mode-hook #'company-mode)
-(add-hook 'cider-repl-mode-hook '(lambda () (linum-mode 0)))
-
-;; Added cider mode for clojure editing
-(add-hook 'clojure-mode-hook #'cider-mode)
 (add-hook 'clojure-mode-hook #'smartparens-mode)
 (add-hook 'clojure-mode-hook #'subword-mode)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook #'turn-on-eldoc-mode)
+
+(defun self/-add-cider-mode ()
+  "Adding cider minor mode for clojure editing"
+
+  (add-to-list 'same-window-buffer-names "<em>nrepl</em>")
+
+  (setq nrepl-log-messages t)
+  (setq nrepl-buffer-name-separator "-")
+  (setq nrepl-popup-stacktraces nil)
+
+  ;; no longer prompt while asking for doc-at-point
+  (setq cider-prompt-for-symbol nil)
+
+  (add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'cider-mode-hook 'smartparens-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+
+  (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'cider-repl-mode-hook 'smartparens-mode)
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook '(lambda () (linum-mode 0)))
+
+  (add-hook 'clojure-mode-hook #'cider-mode))
+
+(self/-add-cider-mode)
 
 ;; TODO:
 ;; unbind C-c C-p p
