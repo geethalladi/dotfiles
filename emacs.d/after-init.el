00611d14a9c40645db757de07e58d447b1726144
geethalladi <geeth.alladi@freshworks.com>
2020-06-06 21:54:57 +0530

    Separte file for font customizations

--- emacs.d/after-init.el
+++ emacs.d/after-init.el
@@ -6,12 +6,7 @@
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(set-face-attribute 'default nil
-                    :family "PT Mono"
-                    :height 160
-                    :weight 'normal
-                    :width 'expanded
-                    :slant 'normal)
+(load "~/.emacs.d/font-init.el")
 
 ;; SAVE BACK TO TEMP FOLDER
 (setq backup-directory-alist '((".*" . "~/.emacs.d/saves")))
---
+++ emacs.d/font-init.el
@@ -0,0 +1,9 @@
+;; Emacs font customizations
+;; Moving this into its own module
+
+(set-face-attribute 'default nil
+                    :family "PT Mono"
+                    :height 160
+                    :weight 'normal
+                    :width 'expanded
+                    :slant 'normal)
