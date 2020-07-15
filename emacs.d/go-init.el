0879d636e0e3df5b5f37c64a8aa7af8370f7f5ba
geethalladi <geeth.alladi@freshworks.com>
2020-07-15 18:36:55 +0530

    Go mode autocomplete

--- emacs.d/go-init.el
+++ emacs.d/go-init.el
@@ -3,6 +3,13 @@
 (require 'go-mode)
 (require 'go-mode-autoloads)
 
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; uses native auto completion (M-TAB)
+;; (depends-on "go-complete")
+;; (add-hook 'completion-at-point-functions 'go-complete-at-point)
 
 (add-hook 'go-mode-hook 'smartparens-mode)
 (add-hook 'go-mode-hook 'projectile-mode)
