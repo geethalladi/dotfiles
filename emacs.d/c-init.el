6c554bbc5655a661229ae207d697a53c1459075f
geethalladi <geeth.alladi@freshworks.com>
2020-07-18 07:36:01 +0530

    C mode customizations

--- emacs.d/c-init.el
+++ emacs.d/c-init.el
@@ -3,3 +3,18 @@
       '((java-mode . "java")
         (c-mode . "linux")
         (other . "gnu")))
+
+(setq-default c-basic-offset 4)
+
+;; (setq c-default-style "linux"
+;;       c-basic-offset 4)
+
+(defun self/c-mode ()
+  "Customizations for C mode"
+  (smartparens-mode)
+  (projectile-mode)
+  (yafolding-mode)
+  (setq indent-tabs-mode t)
+  (setq c-basic-offset 4))
+
+(add-hook 'c-mode-hook 'self/c-mode)
