f68e9cdee0fac19831aa2801397ec2118facc275
geethalladi <geeth.alladi@freshworks.com>
2020-09-19 20:21:49 +0530

    Creating a custom decoder for input sequence

---
+++ emacs.d/terminal-key-decode.el
@@ -0,0 +1,17 @@
+(define self/-custom-decode-map ()
+  "Customize decode input sequences"
+  ;; ctrl keys
+  (define-key input-decode-map "\e[1;5E" [C-return])
+
+  ;; alt keys
+
+  ;; cmd keys
+
+  ;; shift keys
+  (define-key input-decode-map "\e[1;2A" [S-up])
+  (define-key input-decode-map "\e[1;2B" [S-down])
+  (define-key input-decode-map "\e[1;2C" [S-right])
+  (define-key input-decode-map "\e[1;2D" [S-left]))
+
+;; decode the escape sequences inside the terminal
+(self/-custom-decode-map)
