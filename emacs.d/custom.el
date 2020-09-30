d1dd2e2b89ae80b95860b5c4e73a475c565d90d4
geethalladi <geeth.alladi@freshworks.com>
2020-09-30 13:56:25 +0530

    Updating the pandoc output options

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -275,3 +275,21 @@
 
 ;; when bookmarks are added, save it to disk immediately
 (setq bookmark-save-flag 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; pandoc output options
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; default options for all output formats
+(setq org-pandoc-options '((standalone . t)))
+
+;; cancel above settings only for 'docx' format
+(setq org-pandoc-options-for-docx '((standalone . nil)))
+
+;; special settings for beamer-pdf and latex-pdf exporters
+(setq org-pandoc-options-for-beamer-pdf '((pdf-engine . "xelatex")))
+
+(setq org-pandoc-options-for-latex-pdf '((pdf-engine . "pdflatex")))
+
+;; special extensions for markdown_github output
+(setq org-pandoc-format-extensions '(markdown_github+pipe_tables+raw_html))
