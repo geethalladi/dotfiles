5de3bfc4112716c58f6fad4dea892edc203a2d9d
geethalladi <geeth.alladi@freshworks.com>
2020-10-10 12:16:13 +0530

    Refactoring after-init code

--- emacs.d/after-init.el
+++ emacs.d/after-init.el
@@ -16,11 +16,6 @@
 (setq backup-directory-alist '((".*" . "~/.emacs.d/saves")))
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
 
-;; Start the emacs-server
-(load "~/.emacs.d/emacs-server.el")
-
-(load "~/.emacs.d/after-init-load.el")
-
 ;; Never kill scratch buffer
 (add-hook 'kill-buffer-query-functions
     (lambda () (not (equal (buffer-name) "*scratch*"))))
@@ -28,3 +23,8 @@
 ;; delete trailing white space
 ;; (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; Start the emacs-server
+(load "~/.emacs.d/emacs-server.el")
+
+(load "~/.emacs.d/after-init-load.el")
