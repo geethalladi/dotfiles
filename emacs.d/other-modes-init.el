e75af3b7bfdaa6be1026e95f7a31381fc209a36b
geethalladi <geeth.alladi@freshworks.com>
2021-12-13 16:55:02 +0530

    use marginalia

--- emacs.d/other-modes-init.el
+++ emacs.d/other-modes-init.el
@@ -131,3 +131,15 @@
   (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
 
 (global-set-key (kbd "C-c d") 'copy-line)
+
+;; Enable richer annotations using the Marginalia package
+(use-package marginalia
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
