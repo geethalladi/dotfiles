61ffacfb4b52befc1c3a4227c816a6242afe6328
geethalladi <geeth.alladi@freshworks.com>
2021-12-17 12:55:15 +0530

    Elasticsearch daemon function

--- zsh.d/utils.zsh
+++ zsh.d/utils.zsh
@@ -6,3 +6,15 @@ sdkinit () {
         source ${SDKMAN_DIR}/bin/sdkman-init.sh
     fi
 }
+
+# Elasticsearch function [start | stop]
+elasticsearch () {
+    CMD=${1:-"start"}
+    if [[ ${CMD} = "stop" ]]; then
+        # kill the process with the given pid
+        pkill -F ${ES_HOME}/pidfile
+    else
+        # run in daemon mode with pidfile
+        ${ES_HOME}/bin/elasticsearch -d -p ${ES_HOME}/pidfile
+    fi
+}
