93baaedba9dad7949718d6c878eeea802d7cc6b1
geethalladi <geeth.alladi@freshworks.com>
2022-03-03 14:02:40 +0530

    Falling back to bright theme

--- emacs.d/theme-init.el
+++ emacs.d/theme-init.el
@@ -201,7 +201,7 @@
    ;; if dark-mode if true, use it
    ((self/-dark-modep) (self/load-dark-theme))
    ;; else in window system
-   ((window-system) (self/load-dark-theme))
+   ((window-system) (self/load-light-theme))
    ;; fall back to light
    (t (self/load-light-theme))))
 
