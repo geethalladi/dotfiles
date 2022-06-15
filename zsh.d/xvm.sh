f1f55c7d75b6a317731447d488f071e32b2117c6
geethalladi <geeth@inai.io>
2022-06-15 12:54:31 +0530

    Sourcing sdkman as part of xvm

--- zsh.d/xvm.sh
+++ zsh.d/xvm.sh
@@ -13,5 +13,9 @@ if [ "$?" != 0 ]; then
     # rvm
     source ${DOTFILE_DIR_RVM}/rvm.sh
 
+    # sdkman
+    export SDKMAN_DIR="${HOME}/.sdkman"
+    [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+
     export XVM_INIT_COMPLETE=1
 fi
