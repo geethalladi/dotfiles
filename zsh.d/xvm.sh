bf7b9eff3dbb36ed302d271d7b56935b5ffb5135
geethalladi <geeth@inai.io>
2022-06-15 13:01:31 +0530

    Moving sdkman init to end of zshrc

--- zsh.d/xvm.sh
+++ zsh.d/xvm.sh
@@ -13,9 +13,5 @@ if [ "$?" != 0 ]; then
     # rvm
     source ${DOTFILE_DIR_RVM}/rvm.sh
 
-    # sdkman
-    export SDKMAN_DIR="${HOME}/.sdkman"
-    [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
-
     export XVM_INIT_COMPLETE=1
 fi
