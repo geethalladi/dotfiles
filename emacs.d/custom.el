348cf19e603376945b987b88fc859d1c04e572d6
geethalladi <geeth@inai.io>
2022-06-28 18:43:53 +0530

    Mark / go back using mark-ring

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -332,3 +332,22 @@
   (interactive "P")
   (self/threadreader-app
    (self/org-extract-link)))
+
+;; Reference: https://www.masteringemacs.org/article/fixing-mark-commands-transient-mark-mode
+
+(defun self/push-mark-no-activate ()
+  "Pushes `point' to `mark-ring' and does not activate the region
+   Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+  (interactive)
+  (push-mark (point) t nil)
+  (message "Pushed mark to ring"))
+
+(global-set-key (kbd "C-`") 'self/push-mark-no-activate)
+
+(defun self/jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+  This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+
+(global-set-key (kbd "M-`") 'self/jump-to-mark)
