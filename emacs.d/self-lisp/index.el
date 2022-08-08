c053e68d05cafa6285f8aac78ee39bda0029964a
geeth <geethalladi@gmail.com>
2022-08-08 10:17:24 +0530

    Adding Github PR open function

--- emacs.d/self-lisp/index.el
+++ emacs.d/self-lisp/index.el
@@ -14,3 +14,14 @@
   (interactive)
   (browse-url-of-file
    (org-html-export-to-html)))
+
+(defun self/visit-github-pr-url ()
+  "Open the Github's PR creation page"
+  (interactive)
+  (browse-url
+   (let* ((github-pr-uri "https://github.com/%s/pull/new/%s")
+          (current (magit-get-current-branch))
+          (remote (magit-get "remote" (magit-get-push-remote) "url"))
+          (replaced (replace-regexp-in-string "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1" remote))
+          (formatted (format github-pr-uri replaced current)))
+     formatted)))
