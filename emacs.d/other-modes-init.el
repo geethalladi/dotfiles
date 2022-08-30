c6ad0b514dd2f08ca6aafe2358ed1edfabaf6f0e
geeth <geethalladi@gmail.com>
2022-08-30 14:44:51 +0530

    Updating centaur tabs configuration

--- emacs.d/other-modes-init.el
+++ emacs.d/other-modes-init.el
@@ -164,15 +164,17 @@
   (setq centaur-tabs-cycle-scope 'tabs)
   (setq centaur-tabs-set-bar 'under)
   (setq centaur-tabs-set-close-button nil)
+  (setq centaur-tabs-background-color "#777777")
+  (setq centaur-tabs-enable-key-bindings t)
+  (setq centaur-tabs-label-fixed-length 9)
+  (setq centaur-tabs-height 20)
+  (setq centaur-tabs-set-close-button nil)
   (centaur-tabs-mode t)
+
   :bind
   ("C-c t b" . centaur-tabs-backward)
   ("C-c t n" . centaur-tabs-forward))
 
-(setq centaur-tabs-enable-key-bindings t)
-(setq centaur-tabs-label-fixed-length 9)
-(setq centaur-tabs-height 20)
-(setq centaur-tabs-set-close-button nil)
 
 (require 'terraform-mode)
 (require 'terraform-doc)
