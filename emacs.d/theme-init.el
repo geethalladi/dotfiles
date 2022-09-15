bd867c979fe8cc19e73fa2bed6a3bbcd3aed56ff
geeth <geethalladi@gmail.com>
2022-09-15 12:33:37 +0530

    No transparency in window system

--- emacs.d/theme-init.el
+++ emacs.d/theme-init.el
@@ -76,8 +76,9 @@
   (set-face-attribute 'helm-selection nil
                       :background "green"
                       :foreground "black")
-  (set-frame-parameter (selected-frame) 'alpha '(85 85))
-  (add-to-list 'default-frame-alist '(alpha 85 85))
+  (when (not (window-system))
+    (set-frame-parameter (selected-frame) 'alpha '(85 85))
+    (add-to-list 'default-frame-alist '(alpha 85 85)))
 
   (custom-set-faces
    ;; org mode faces
