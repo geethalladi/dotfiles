f59e3ee76bae13e503a08806d97f291e1052ac44
geethalladi <geethalladi@gmail.com>
2023-03-15 19:42:42 +0530

    Committing changes to emacs+ custom.el

--- emacsplus.d/custom.el
+++ emacsplus.d/custom.el
@@ -3,8 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(undo-fu lsp-java))
  '(warning-suppress-log-types '((doom-first-file-hook) (defvaralias)))
- '(warning-suppress-types '((doom-first-file-hook) (defvaralias))))
+ '(warning-suppress-types
+   '((doom-first-buffer-hook)
+     (doom-first-file-hook)
+     (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
