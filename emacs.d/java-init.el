0864a9abfa87298618962480822b0dae742b3fc6
geethalladi <geethalladi@gmail.com>
2023-03-18 17:27:34 +0530

    Add java init file

---
+++ emacs.d/java-init.el
@@ -0,0 +1,34 @@
+;; (condition-case nil
+;;     (require 'use-package)
+;;   (file-error
+;;    (require 'package)
+;;    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;;    (package-initialize)
+;;    (package-refresh-contents)
+;;    (package-install 'use-package)
+;;    (setq use-package-always-ensure t)
+;;    (require 'use-package)))
+
+;; (use-package projectile)
+;; (use-package flycheck)
+;; (use-package yasnippet :config (yas-global-mode))
+;; (use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration)))
+;; (use-package hydra)
+;; (use-package company)
+;; (use-package lsp-ui)
+;; (use-package which-key :config (which-key-mode))
+;; (use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+
+;; (require 'dap-mouse)
+;; (require 'dap-java)
+;; (require 'lsp-modeline)
+;; (use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+;; (dap-mode 1)
+;; (dap-tooltip-mode 1)
+;; (require 'dap-ui)
+
+;; (use-package dap-java :ensure nil)
+;; (use-package helm-lsp)
+;; (use-package helm
+;;   :config (helm-mode))
+;; (use-package lsp-treemacs)
