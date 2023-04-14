d0e5a8a2919e2ddde1c436b622cb547c19f4cca0
geethalladi <geethalladi@gmail.com>
2023-04-14 13:29:43 +0530

    Playing with java mode in emacs

--- emacs.d/java-init.el
+++ emacs.d/java-init.el
@@ -14,10 +14,23 @@
        :config (helm-mode))
 (use-package lsp-treemacs)
 
+;; (setq lsp-java-jdt-download-url
+;;       "https://download.eclipse.org/jdtls/milestones/0.57.0/jdt-language-server-0.57.0-202006172108.tar.gz")
+
+;; (setq lsp-java-vmargs
+;;       '("-noverify" "-Xmx3G" "-XX:+UseG1GC" "-XX:+UseStringDeduplication"
+;;         "-javaagent:/Users/galladi/.m2/repository/org/projectlombok/lombok/1.18.20/lombok-1.18.20.jar"
+;;         "-Xbootclasspath/a:/Users/galladi/.m2/repository/org/projectlombok/lombok/1.18.20/lombok-1.18.20.jar"))
+
+;; (use-package lsp-sonarlint)
+;; (use-package lsp-sonarlint-java)
+;; (setq lsp-sonarlint-java-enabled t)
+
 (defun self/java-mode ()
   "Java mode customizations"
   (setq c-basic-offset 2)
   (smartparens-mode 1)
+  ;; (lsp-sonarlint-java)
   (setq line-spacing 0.2))
 
 (add-hook 'java-mode-hook 'self/java-mode)
