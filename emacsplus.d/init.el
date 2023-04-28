a09a6293b6f0a52e69bab6574195dfcd7a3cb6dd
geethalladi <geethalladi@gmail.com>
2023-04-28 15:20:05 +0530

    Using plantuml checkers

--- emacsplus.d/init.el
+++ emacsplus.d/init.el
@@ -155,7 +155,7 @@
        ocaml             ; an objective camel
        org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
-       plantuml          ; diagrams for confusing people more
+       (plantuml :checkers) ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
