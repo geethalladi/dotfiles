ccd6a8e37ed91f2e308e1f1ba18283fef357f2f5
geethalladi <geethalladi@gmail.com>
2023-12-06 16:15:52 +0530

    Removing scheme/guile for now

--- emacsplus.d/init.el
+++ emacsplus.d/init.el
@@ -166,7 +166,7 @@
        (ruby +rails +rvm +lsp)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme +guile)   ; a fully conniving family of lisps
+       ;; (scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
