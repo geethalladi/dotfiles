8bb4ab7598396ae2a8ae37ed02775f7869b436af
geethalladi <geethalladi@gmail.com>
2023-12-26 20:30:41 +0530

    Creating docker-init file

---
+++ emacs.d/docker-init.el
@@ -0,0 +1,10 @@
+;;; docker-init.el --- Description -*- lexical-binding: t; -*-
+;;
+
+(use-package docker)
+(use-package docker-api)
+(use-package docker-tramp)
+(use-package dockerfile-mode)
+(use-package docker-compose-mode)
+(use-package docker-tramp)
+
