ee2657f0f3ef2bf4e32d73c34b65c8bfa3ddd74c
geethalladi <geethalladi@gmail.com>
2023-12-26 13:29:38 +0530

    Comment out copilot configuration in emacsplus

--- emacsplus.d/config.el
+++ emacsplus.d/config.el
@@ -120,12 +120,12 @@
 (require 'lsp-sonarlint-java)
 (setq lsp-sonarlint-java-enabled t)
 
-(use-package! copilot
-  :hook (prog-mode . copilot-mode)
-  :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+;; (use-package! copilot
+;;   :hook (prog-mode . copilot-mode)
+;;   :bind (:map copilot-completion-map
+;;               ("<tab>" . 'copilot-accept-completion)
+;;               ("TAB" . 'copilot-accept-completion)
+;;               ("C-TAB" . 'copilot-accept-completion-by-word)
+;;               ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
