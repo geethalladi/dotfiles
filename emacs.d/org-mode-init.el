e624f15c30344628149f31169a853427a3c433a4
geethalladi <geethalladi@gmail.com>
2024-01-17 18:15:54 +0530

    Comment out straight + org-contrib

--- emacs.d/org-mode-init.el
+++ emacs.d/org-mode-init.el
@@ -1,26 +1,26 @@
 ;; using straight
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name
+;;         "straight/repos/straight.el/bootstrap.el"
+;;         (or (bound-and-true-p straight-base-dir)
+;;             user-emacs-directory)))
+;;       (bootstrap-version 7))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
 
-(straight-use-package 'org)
+;; (straight-use-package 'org)
 
-;; installing org-contrib via straight
-(straight-use-package '(org-contrib :type git
-                                    :repo "https://git.sr.ht/~bzg/org-contrib";
-                                    :local-repo "org-contrib"))
+;; ;; installing org-contrib via straight
+;; (straight-use-package '(org-contrib :type git
+;;                                     :repo "https://git.sr.ht/~bzg/org-contrib";
+;;                                     :local-repo "org-contrib"))
 (require 'org)
 (require 'org-bullets)
 (require 'org-mime)
