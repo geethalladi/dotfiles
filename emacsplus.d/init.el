585a98e4442a0ae62fca53a28676918953791e67
Geeth Alladi <geeth.alladi@freshworks.com>
2024-07-15 17:37:14 +0530

    Updating python config in doom

--- emacsplus.d/init.el
+++ emacsplus.d/init.el
@@ -155,7 +155,8 @@
        ;;php               ; perl's insecure younger brother
        (plantuml :checkers) ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       ;;(python +conda +lsp +poetry +pyenv +pyright +tree-sitter)
+       (python +lsp +pyenv +poetry +tree-sitter)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
