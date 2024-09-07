6aad4966f691e4e957386eb4c475d6005dd2d35c
Geeth Alladi <geeth.alladi@freshworks.com>
2024-09-07 20:42:39 +0530

    Updating pryrc with alias_commands

--- ruby.d/pryrc.rb
+++ ruby.d/pryrc.rb
@@ -9,4 +9,9 @@ if Pry::Prompt[:rails]
   Pry.config.prompt = Pry::Prompt[:rails]
 end
 
+Pry.commands.alias_command 'c', 'continue'
+Pry.commands.alias_command 's', 'step'
+Pry.commands.alias_command 'n', 'next'
+Pry.commands.alias_command 'h', 'help'
+
 # Updates from https://github.com/JuanitoFatas/dotpryrc/blob/master/.pryrc
