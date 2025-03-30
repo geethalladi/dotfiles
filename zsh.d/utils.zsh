110ddabc0cb4624eeec122ceb9ccbbd8ceb993ac
geethalladi <geethalladi@gmail.com>
2025-03-30 18:33:27 +0530

    Added conda init

--- zsh.d/utils.zsh
+++ zsh.d/utils.zsh
@@ -33,3 +33,20 @@ emacspipe () {
 generate_ripper_tags () {
     ripper-tags -R -f TAGS
 }
+
+conda_init () {
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/usr/local/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+            . "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+        else
+            export PATH="/usr/local/Caskroom/miniconda/base/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
+}
