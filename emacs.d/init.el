f96cccb490a33d6dc7ea04c871870d4f86d8b8e3
geethalladi <geethalladi@gmail.com>
2025-12-29 18:41:09 +0530

    Commenting out straight.el + org-modern

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,3 +2,22 @@
 
 (add-hook 'after-init-hook
           (lambda () (load (expand-file-name "after-init.el" user-emacs-directory))))
+
+;; ;; installing straight
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name
+;;         "straight/repos/straight.el/bootstrap.el"
+;;         (or (bound-and-true-p straight-base-dir)
+;;             user-emacs-directory)))
+;;       (bootstrap-version 7))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
+
+;; ;; (setq package-enable-at-startup nil)
